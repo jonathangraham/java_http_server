c4f15ae7b0a6d8a4b5960a6065a871093819a60a
Jonathan Graham <sendthistojon@gmail.com>
2015-09-29 12:03:52 -0500

    add ServerConfig class to allow command line argument options with default settings

--- src/main/java/com/jgraham/httpserver/App.java
+++ src/main/java/com/jgraham/httpserver/App.java
@@ -1,18 +1,55 @@
 package com.jgraham.httpserver;
 
+import com.jgraham.httpserver.ServerConfig;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.Date;
 
+
+//class App {
+//    public static void main(String args[]) throws IOException {
+//
+//        int port = 0;
+//
+//        try {
+//            port = Integer.parseInt(args[0]);
+//        }
+//        catch (NumberFormatException nfe) {
+//            port = 5000;
+//        }
+//
+//        ServerSocket server = new ServerSocket(port);
+//        System.out.println("Listening for connection on port " + port);
+//        while (true) {
+//            try (Socket socket = server.accept()) {
+//                Date today = new Date();
+//                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+//                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+//            }
+//        }
+//    }
+//}
+
 class App {
     public static void main(String args[]) throws IOException {
-        ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080 ....");
+
+        ServerConfig.configureServer(args);
+
+        int port = ServerConfig.getPort();
+        String directory = ServerConfig.getDirectory();
+
+        ServerSocket server = new ServerSocket(port);
+        System.out.println("Listening for connection on port " + port);
+
         while (true) {
             try (Socket socket = server.accept()) {
                 Date today = new Date();
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                String httpResponse =   "HTTP/1.1 200 OK\r\n\r\n"
+                                        + "date: " + today
+                                        + "\r\nport: " + port
+                                        + "\r\ndirectory: " + directory;
                 socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
